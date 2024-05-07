ce4acf619b601f731a25652814e4a6cfa62eb0b6
Semyon Likhorad <63965069+xianery@users.noreply.github.com>
2024-05-07 20:39:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pymem.memory
+
+offsets = {
+    "dwLocalPlayer": int(0x6D6870),
+    "m_flFlashMaxAlpha": int(0x9CE0),
+}
+
+csso = pymem.Pymem('hl2.exe')
+client = pymem.process.module_from_name(csso.process_handle, 'client.dll').lpBaseOfDll
+
+localplayer = csso.read_uint(client + offsets['dwLocalPlayer'])
+flashstrength = localplayer + offsets['m_flFlashMaxAlpha']
+
+csso.write_float(flashstrength, float(0) if csso.read_float(flashstrength) == float(255) else float(255))
